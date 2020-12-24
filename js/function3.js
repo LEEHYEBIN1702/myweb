c88dc1008043f361641a902f965c35fef526fb43
lee hyebin <qls1702@hanmail.net>
2020-12-24 12:40:23 +0900

    a

--- js/function3.js
+++ js/function3.js
@@ -2,25 +2,27 @@ let friends = [];
 let i = 0; 
 // 전역변수 한번 선언했기 때문에 인풋 프렌드 펑션이 호출 될 때 마다 증가함.
 
-function input_freind() {
-    let name = document.getElementById('name').value;
-    let age = document.getElementById('age').value;
-    let hobby = document.getElementById('hobby').value;
+// function input_freind() {
+//     let name = document.getElementById('name').value;
+//     let age = document.getElementById('age').value;
+//     let hobby = document.getElementById('hobby').value;
+
     // console.log(name.value, age.value, hobby.value);
 
-    let friend = {};
-    friend.name = name;
-    friend.age = age;
-    friend.hobby = hobby;
 
-    friends[i++] = friend;
-    // console.clear();
+//     let friend = {};
+//     friend.name = name;
+//     friend.age = age;
+//     friend.hobby = hobby;
 
- for (friend of friends) {
-        for(field in friend) {
-            console.log(friend[field]);
-        }
-    }
+//     friends[i++] = friend;
+//     // console.clear();
+
+//  for (friend of friends) {
+//         for(field in friend) {
+//             console.log(friend[field]);
+//         }
+//     }
 
 
     // document.getElementById('show').innerHTML= name.value+','+age.value+','+hobby.value;
@@ -28,3 +30,14 @@ function input_freind() {
 }
 // 클릭하는 이벤트가 발생할 때 펑션을 실행하겠다.
 
+function input_freind() {
+    let name = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+    let hobby = document.getElementById('hobby').value;
+
+    let liTag = document.createElement('li');
+    liTag.innerHTML = name, age, hobby;
+    document.getElementById('name, age, hobby').append(liTag);
+
+    let ulTag = document.createElement('ul');
+    ulTag.id = 'friends';
