44b94dd4523bfbbb9745b6add6891b6d22a13615
lee hyebin <qls1702@hanmail.net>
2020-12-24 10:54:35 +0900

    1224

--- js/function2.js
+++ js/function2.js
@@ -54,20 +54,85 @@
 // }
 
 
-let numAry = [];
-for (let i = 0; i < 5; i++) {
-let name2 = window.prompt("숫자를 입력하세요.");
-numAry [i] = parseInt(name1);
-}
+// let numAry = [];
+// for (let i = 0; i < 5; i++) {
+// let name2 = window.prompt("숫자를 입력하세요.");
+// numAry [i] = parseInt(name1);
+// }
 
-findMax(name1, name2); 
+// findMax(name1, name2); 
 
-    function findMax(a, b) {
-        let n1 = parseInt(a); 
-        let n2 = parseInt(b);
-        if (15 <n1+n2< 25) {
-            console.log (n1+n2);
-        } else {
-            console.log();
-        }
+//     function findMax(a, b) {
+//         let n1 = parseInt(a); 
+//         let n2 = parseInt(b);
+//         if (15 <n1+n2< 25) {
+//             console.log (n1+n2);
+//         } else {
+//             console.log();
+//         }
+//     }
+
+    // let numAry = [];
+    // for (let i = 0; i < 5; i++) {
+    //     let name1 = window.prompt('숫자를 입력하세요.');
+    //     numAry[i] = parseInt(name1);
+    // }
+    // findMax();
+
+    // function findMax() {
+    //     //numAry[]
+    //     let max= 0;
+    //     for (val of numAry) {
+    //         if (max < val) {
+    //             max = val
+    //         }
+    //     }
+    // }
+    // console.log("최대값: " + max);
+   
+    // let friend1 = {}
+    // friend1.name = 'Hong';
+    // friend1.age = 20;
+    // friend3.hobby = 'reading';
+
+    // let friend2 = {
+    //     name : 'Hwang',
+    //     age : 20
+    // }
+
+//     let friends = [friend1, friend2];
+//     for (friend of friends) {
+//     for (field in friend) {
+//         console.log(friend[field]);
+//     }
+// }
+
+// let friends = [];
+// friends[0] = friend1
+// friends[1] = friend2
+// let numAry = [];
+
+
+
+
+ let friends = []; // 배열
+    for (let i = 0; i < 3; i++) {
+        let name = window.prompt('이름을 입력하세요.');
+        let age = window.prompt('나이를 입력하세요.');
+        let friend = {}; // 오브젝트
+        friend.name = name;
+        friend.age = age;
+        friends[i] = friend;
     }
+    document.write('<table border="1"><tr>');
+    document.write('<tr><td>이름</td><td>나이</td></tr>')
+    for (friend of friends) {
+        document.write('<tr>'); // 받아진 이름,나이 행만큼 줄이 만들어져야 하니까 밖에 써주는 것.
+        document.write('<tr align= "center">')
+        for (field in friend) {
+            document.write('<td>' + friend[field] + '</td>')
+        }
+        document.write('</tr>');
+    }
+    document.write('</table>');
+    
---
+++ js/function3.js
@@ -0,0 +1,10 @@
+function input_freind() {
+    let namef = document.getElementById('name');
+    let agef = document.getElementById('age');
+    let hobbyf = document.getElementById('hobby');
+    console.log(namef.value, agef.value, hobbyf.value);
+
+}
+
+// 클릭하는 이벤트가 발생할 때 펑션을 실행하겠다.
+
